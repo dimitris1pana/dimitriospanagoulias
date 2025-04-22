cfaa9b1c2868f122a1f7c94de69713fab91e72f6
dimitris1pana <panagoulias_d@yahoo.gr>
2025-04-22 17:12:39 +0300

    HomeRmv

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // src/App.js
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import './App.css';
