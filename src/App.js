b034cf3a1519f9719e8425cee6f3f935fcd91068
dimitris1pana <panagoulias_d@yahoo.gr>
2025-04-22 20:34:37 +0300

    _theme_

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Projects from './components/Projects';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme])
+
   return (
     <div>
-      <Navbar />
+      <Navbar onToggleTheme={toggleTheme} currentTheme={theme} />
       <Routes>
         <Route path="/" element={<About />} />
         <Route path="/projects" element={<Projects />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 // src/components/Navbar.js
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <nav className="bg-gray-800 p-4 text-white">
+const Navbar = ({ onToggleTheme, currentTheme }) => (
+    <nav className="navbar" style={{ display: 'flex', alignItems: 'center' }}>
 <ul className="no-bullets">
 <li><h1><Link to="/">🌀About</Link><Link to="/projects">💾Projects</Link></h1></li>
  
     </ul>
+    <button className="toggle-button" onClick={onToggleTheme}>
+      {currentTheme === 'light' ? '🌙 Dark Mode' : '☀️ Light Mode'}
+    </button>
   </nav>
 );
 
