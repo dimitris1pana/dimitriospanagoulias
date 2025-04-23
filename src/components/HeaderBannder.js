df9b7256c62f4c6d9649e168bf4b948fefa0238b
dimitris1pana <panagoulias_d@yahoo.gr>
2025-04-23 10:51:48 +0300

    NavButtons_favtext_

--- src/components/HeaderBannder.js
+++ src/components/HeaderBannder.js
@@ -6,6 +6,13 @@ const HeaderBanner = ({ theme }) => {
     <div className={`header-banner ${theme}`}>
       <h1 className="name">Dimitrios P. Panagoulias, PhD</h1>
       <p className="subtitle">AI Researcher <span>|</span> MLOps Specialist</p>
+      <div className="social-links">
+        <a href="https://www.linkedin.com/in/dimitris-panagoulias-17a05217/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <span> | </span>
+        <a href="https://diskinside.com" target="_blank" rel="noopener noreferrer">Website</a>
+        <span> | </span>
+        <a href="https://github.com/dimitris1pana" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 // src/components/Navbar.js
 import { Link } from 'react-router-dom';
 
+
 const Navbar = ({ onToggleTheme, currentTheme }) => (
     <nav className="navbar" style={{ display: 'flex', alignItems: 'center' }}>
 <ul className="no-bullets">
-<li><h1><Link to="/">🌀About</Link><Link to="/projects">💾Projects</Link></h1></li>
+<li> <div className="nav-buttons">
+          <Link to="/" className="nav-button">
+            <span>🌀</span>About
+          </Link>
+          <Link to="/projects" className="nav-button">
+            <span>💾</span>Projects
+          </Link>
+        </div></li>
 
     </ul>
     <button className="toggle-button" onClick={onToggleTheme}>
